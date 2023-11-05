2991d6e90bcbabec8b4f7dc14245c3aa6a30036c
EmordiGoddowell <goddowellemordi1147@gmail.com>
2023-11-05 11:21:46 -0800

    getline_strtok

---
+++ getline_strtok.c
@@ -0,0 +1,93 @@
+#include "shell.h"
+
+/**
+ * get_line - Reads input from standard input by user
+ * Return: The input on a buffer
+**/
+
+char *get_line()
+{
+	int i, rd, buffsize = BUFSIZE;
+	char c = 0, *buffer, *buf;
+
+	buffer = malloc(buffsize);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	for (i = 0; c != EOF && c != '\n'; i++)
+	{
+		fflush(stdin);
+		rd = read(STDIN_FILENO, &c, 1);
+		if (rd == 0)
+		{
+			free(buffer);
+			exit(EXIT_SUCCESS);
+		}
+		buffer[i] = c;
+		if (buffer[0] == '\n')
+			return (enter(buffer));
+		if (i >= buffsize)
+		{
+			buffer = realloc(buffer, (buffsize + 2));
+			if (buffer == NULL)
+			{
+				free(buffer);
+				return (NULL);
+			}
+		}
+	}
+	buffer[i] = '\0';
+	buf = space(buffer);
+	free(buffer);
+	hashtag_handler(buf);
+	return (buf);
+}
+
+/**
+ * _strtok - function that extracts tokens of a string
+ * @str: string
+ * @delim: delimiter
+ * Return: pointer to the next token or NULL
+**/
+
+char *_strtok(char *str, const char *separate)
+{
+	static char *tokens;
+	static char *new_token;
+	unsigned int i;
+
+	if (str != NULL)
+		new_token = str;
+	tokens = new_token;
+	if (tokens == NULL)
+		return (NULL);
+	for (i = 0; tokens[i] != '\0'; i++)
+	{
+		if (check_delim(tokens[i], delim) == 0)
+			break;
+	}
+	if (new_token[i] == '\0' || new_token[i] == '#')
+	{
+		new_token = NULL;
+		return (NULL);
+	}
+	tokens = new_token + i;
+	new_token = tokens;
+	for (i = 0; new_token[i] != '\0'; i++)
+	{
+		if (check_delim(new_token[i], delim) == 1)
+			break;
+	}
+	if (new_token[i] == '\0')
+		new_token = NULL;
+	else
+	{
+		new_token[i] = '\0';
+		new_token = new_token + i + 1;
+		if (*new_token == '\0')
+			new_token = NULL;
+	}
+	return (tokens);
+}
