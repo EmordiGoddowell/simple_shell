843165b6713f508065eac92f2c2c9b1300996153
EmordiGoddowell <goddowellemordi1147@gmail.com>
2023-11-05 11:32:18 -0800

    shell

--- shell.h
+++ shell.h
@@ -5,9 +5,52 @@
 #define SEPARATOR " \t\r\n\a"
 #define OUTPUT(c) (write(STDERR_FILENO, c, _strlen(c)))
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 /** error_functions.c **/
 void print_error_message(char *input, int counter, char **argv);
 void custom_error(char **argv, int c, char **cmd);
 void file_error_handler(char **argv, int c);
+
+/** 0-string_io_functions.c **/
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _strncmp(const char *s1, const char *s2, size_t n);
+
+/** 1-string_io_function.c **/
+int _putchar(char c);
+void _puts(char *str);
+int _atoi(char *s);
+char *_strchr(char *s, char c);
+char *_strdup(char *str);
+
+/** 2-string_io_function.c **/
+int _isalpha(int c);
+void flip_array(char *arr, int len);
+int int_count(int num);
+char *uint_char(unsigned int n);
+int _strcmp(char *s1, char *s2);
+
+/** command_operations.c **/
+int find_cmd(char **cmd);
+char *build_path(char *token, char *value);
+char *get_env(char *name);
+
+/** input_handling.c **/
+char *new_line(char *string);
+char *white_space(char *str);
+void hashtag(char *buff);
+unsigned int check_delim(char c, const char *str);
+
+/** parse_command.c **/
+char **parse_command(char *input);
+
+/** getline_strtok.c **/
+char *get_line();
+char *_strtok(char *str, const char *separate);
 
 #endif /** SHELL_H **/
