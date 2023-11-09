b0e07bb760332e2fc327c2152716bf709aeb4d25
EmordiGoddowell <goddowellemordi1147@gmail.com>
2023-11-08 16:19:59 -0800

    shell.h

--- shell.h
+++ shell.h
@@ -9,10 +9,12 @@ extern char **environ;
 #include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <limits.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
